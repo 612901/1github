ce421f438cf6ea35023fdc48a67a75b4cb8d74cf
wuying <106313976+612901@users.noreply.github.com>
2023-01-18 14:05:52 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ from http.server import BaseHTTPRequestHandler
 from bs4 import BeautifulSoup
 
 def github_json(user,repo,branch):
-    if user =='':
+    if user =='612901':
         result = 'The user cannot be none!'
     else:
         try:
